60077c7e3e82c38271db17e37cff547031786b86
Ivan <cherepov.iv@yandex.ru>
2020-04-27 12:50:08 +0300

    del

--- Les4.2v1.py
+++
@@ -1,5 +0,0 @@
-user_list = input('Введите числа через пробел: ').split()
-my_list = [int(i) for i in user_list]
-new_list = [my_list[i] for i in range(1, len(my_list)) if my_list[i] > my_list[i - 1]]
-print(my_list)
-print(new_list)
